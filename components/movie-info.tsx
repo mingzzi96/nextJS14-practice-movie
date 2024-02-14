4514bb4d5bc18a66812fbf061fb03287bbd9a58a
mingming <jimin23623@gmail.com>
2024-02-14 22:57:57 +0900

    💄 style: video 모음집들 스타일 입히기

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,3 +1,5 @@
+import styles from "../styles/movie-info.module.css";
+
 async function getMovie(id: string) {
   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/${id}`);
   return response.json();
@@ -6,5 +8,21 @@ async function getMovie(id: string) {
 // 영화 정보의 ui만 가진 코드
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={styles.container}>
+      <img
+        src={movie.poster_path}
+        alt={movie.title}
+        className={styles.poster}
+      />
+      <div>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+        <p className={styles.info}>{movie.overview}</p>
+        <a href={movie.hompage} target={"_blank"}>
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,3 +1,5 @@
+import styles from "../styles/movie-videos.module.css";
+
 async function getVideos(id: string) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/${id}/videos`
@@ -8,5 +10,17 @@ async function getVideos(id: string) {
 // video의 ui만 가진 코드
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          title={video.name}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      ))}
+    </div>
+  );
 }
