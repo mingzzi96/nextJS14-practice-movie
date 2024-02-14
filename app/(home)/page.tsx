057a635e41db1428f8e76199e5b12e2157817a73
mingming <jimin23623@gmail.com>
2024-02-14 21:35:04 +0900

    ♻️  동시에 api fetch 요청할 수 있게 리펙토링

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,7 @@
 // server component
 
+import Link from "next/link";
+
 // "use client" 필요 없음!
 
 export const metadata = {
@@ -7,21 +9,29 @@ export const metadata = {
 };
 
 async function getMovies() {
-  // 로딩 상태를 위해 일부러 프로그램을 느리게 만들어주는 트릭
+  // 로딩 상태를 위해 일부러 프로그램을 느리게 만들어주는 트릭 (실제로는 필요 없는 코드)
   // getMovies 함수가 이 구문에서 2초간 멈췄다가 실행될 것이다.
   // 그럼 사용자는 화면이 로딩될 때 까지 접근이 불가능하다.
   // 하지만!! loading.tsx라는 파일을 page.tsx와 동일한 경로에 만들어주면
   // next가 알아서 자동으로 해당 컴포넌트를 화면에 로딩이 완료될때 까지 띄워준다.
-  await new Promise((resolve) => setTimeout(resolve, 500));
+  // await new Promise((resolve) => setTimeout(resolve, 500));
 
   // 라이브러리 필요 없이 자동으로 url을 fetch해 준다.
   // 첫 fetch때만 API에 요청을 한다. 그리고 그 결과 상태를 기억해 두었다가 다음 부터는 재사용한다. 그래서 빨라보임
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/movies`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}`);
   const json = await response.json();
   return json;
 }
 
 export default async function HomePage() {
   const movies = await getMovies();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,9 +1,24 @@
+async function getMovie(id: string) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/${id}/videos`
+  );
+  return response.json();
+}
+
 // params 말고 props를 넣어서 확인해 보면 객체 안에 params와 searchParams가 들어 있는 것을 확인할 수 있다.
 // react와 다르게 hook을 이용할 필요 없이 next 자체에서 제공한다.
-export default function MovieDetail({
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>movie {id}</h1>;
+  // Promise.all을 통해 여러개의 api를 동시에 fetch해 올 수 있다.
+  // 이렇게 해주지 않고 따로 작성하게 되면 순서대로 호출을 기다려야만한다.
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  return <h1>{movie.title}</h1>;
 }
