e3cc6dfe8d2e1637987dfc7abce4e6e5b0769923
mingming <jimin23623@gmail.com>
2024-02-14 15:51:06 +0900

    ✨ feat: useState를 사용하여 fetch 구현하기

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,18 @@
-export const metadata = {
-  title: "HOME",
-  description: "The best movies on the best framework",
-};
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/movies`);
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
 }
